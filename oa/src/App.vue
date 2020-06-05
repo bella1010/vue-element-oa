38714d42bbee519356536fd374a22fae4db3ee94
BellaChen <oncwnuMD_8-GDFTolw3uvUEmVB1M@git.weixin.qq.com>
2020-06-05 15:09:19 +0800

    init

---
+++ oa/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <work-flow></work-flow>
+  </div>
+</template>
+
+<script>
+  import WorkFlow from './components/WorkFlow.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      WorkFlow
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
